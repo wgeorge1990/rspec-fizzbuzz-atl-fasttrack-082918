6bf32c2b9678b7e33dc8ee6d2d1bfb9428e7766c
william george <williameverettgeorge@icloud.com>
2018-09-29 19:30:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,19 @@
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
-  else int % 5 == 0 # if the number int is divisible by 3
-    "Buzz" # Go fizz
+  if int % 3 == 0 && % 5 == 0
+    "FizzBuzz"
   end
 end
+
+
+
+
+
+
+#def fizzbuzz(int)
+#  if int % 3 == 0 # if the number int is divisible by 3
+  #  "Fizz" # Go fizz
+#  else int % 5 == 0 # if the number int is divisible by 3
+#    "Buzz" # Go fizz
+#  end
+#end
